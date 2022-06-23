493d246de0b1f8feda268ccac1c84d06fbd06f2a
Charliep938 <69971399+Charliep938@users.noreply.github.com>
2022-06-23 21:52:45 +0100

    php subject

--- emailform.php
+++ emailform.php
@@ -8,7 +8,7 @@
       $userName = $_POST['name'];
       $userEmail = $_POST['email'];
       $message = $_POST['message'];
-      $messageSubject = $_POST['A new form has been submitted'];
+      $subject - $_POST['Subject test'];
     
       $to ="cpates93@gmail.com";
       $body = "";
