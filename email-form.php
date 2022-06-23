fdfa03616a05c889cdd421b143969dafbe6de46e
Charliep938 <69971399+Charliep938@users.noreply.github.com>
2022-06-23 19:46:16 +0100

    added php submit form

---
+++ email-form.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("cpates93@gmail.com", "Contact from charliepates.online",
+
+
+$_POST["Thank you for contacting Charlie"]. "From: cpates93@gmail.com");
+
+
+}
+
+
+?>
