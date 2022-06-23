e845c45648c9b0a9719590ec70919d477fbb41a2
Charliep938 <69971399+Charliep938@users.noreply.github.com>
2022-06-23 21:08:19 +0100

    php update

--- emailform.php
+++ emailform.php
@@ -26,7 +26,28 @@
 
 
 if($message_sent = true;) {
+  <!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Charlie Pates</title>
+  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css" />
+
+</head>
+
+<body>
   <div class="thanks">
     thanks!
   </div>
+</body>
+</html>
 }
